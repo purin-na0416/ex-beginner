36e55ab0677d6df26171434a74bbb18b980e3a67
Natsuki Fujii <natsuki.fujii@rakus-partners.co.jp>
2023-08-21 15:12:10 +0900

    UserForm.java修正(入力値チェックを追加)

--- src/main/java/com/example/exbeginner/form/UserForm.java
+++ src/main/java/com/example/exbeginner/form/UserForm.java
@@ -1,8 +1,16 @@
 package com.example.exbeginner.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class UserForm {
+  @NotBlank(message = "名前は必須です")
   private String name;
+
+  @NotNull(message = "年齢は必須です")
   private Integer age;
+
+  @NotBlank(message = "コメントは必須です")
   private String comment;
   
   public String getName() {
