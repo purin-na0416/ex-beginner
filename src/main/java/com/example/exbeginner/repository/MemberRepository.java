e20132446b269bf0ecbc9fc4a2336fca83cbb05b
Natsuki Fujii <natsuki.fujii@rakus-partners.co.jp>
2023-08-21 16:31:42 +0900

    MemberRepository.java作成

---
+++ src/main/java/com/example/exbeginner/repository/MemberRepository.java
@@ -0,0 +1,42 @@
+package com.example.exbeginner.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.exbeginner.domain.Member;
+
+@Repository
+public class MemberRepository {
+  @Autowired
+  NamedParameterJdbcTemplate template;
+
+  private static final RowMapper<Member> MEMBER_ROW_MAPPER = (rs, i) -> {
+    Member member = new Member();
+    member.setId(rs.getInt("id"));
+    member.setName(rs.getString("name"));
+    member.setAge(rs.getInt("age"));
+    member.setDep_id(rs.getInt("dep_id"));
+    return member;
+  };
+
+  public List<String> search(String name) {
+    String sql = "select id, name, age, dep_id from members where name like :name;";
+    SqlParameterSource param = new MapSqlParameterSource().addValue("name", '%' + name + '%');
+
+    List<Member> memberList = template.query(sql, param, MEMBER_ROW_MAPPER);
+    
+    List<String> nameList = new ArrayList<>();
+    for(Member member : memberList) {
+      nameList.add(member.getName());
+    }
+
+    return nameList;
+  }
+}
